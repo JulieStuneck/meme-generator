423fa2db66c90b3d48e889a51a9951da42d69227
JulieStuneck <jbstuneck@yahoo.com>
2018-12-21 20:50:00 -0500

    Updates

---
+++ precache-manifest.b7cd1ecb8be2af52b05ab1e3b82cfa85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "340a140dc48e109e29a4",
+    "url": "/meme-generator/static/js/runtime~main.340a140d.js"
+  },
+  {
+    "revision": "3c85e7747a7a67ad54bc",
+    "url": "/meme-generator/static/js/main.3c85e774.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/meme-generator/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "3c85e7747a7a67ad54bc",
+    "url": "/meme-generator/static/css/main.cf4918b8.chunk.css"
+  },
+  {
+    "revision": "2ac84517c354321699c9a3196916f67d",
+    "url": "/meme-generator/index.html"
+  }
+];
