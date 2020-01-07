28e242e50e199bad8e242b85b22fd115bbd2adfd
JulieStuneck <jbstuneck@yahoo.com>
2020-01-07 10:57:01 -0500

    updating dependencies

--- src/ImagePicker.js
+++ src/ImagePicker.js
@@ -5,14 +5,27 @@ import './ImagePicker.css';
 class ImagePicker extends Component {
     render() {
         const { selectImage } = this.props;
+        //const {showSelected} = this.props;
+
+        let className = '';
+
+       /* if (this.props.selectImage) {
+            className = 'selectedPic';
+        } else {
+            className = '';
+        }*/
+
+        this.props.selectImage ? className = 'selectedPic' : className = '';
+        !this.props.selectImage ? className = 'selectedPic' : className = '';
+        
         return (
             <div className="imagePicker">
                 <h2>Select a picture:</h2>
                 <div className="imageChoices">
-                    <img src="Images/Bird.jpg" alt="bird" className="selectedPic" onClick={selectImage} />
-                    <img src="Images/Cat.jpg" alt="cat" className="selectedPic" onClick={selectImage} />
-                    <img src="Images/Dog.jpg" alt="dog" className="selectedPic" onClick={selectImage} />
-                    <img src="Images/Girl.jpg" alt="girl" className="selectedPic" onClick={selectImage} />
+                    <img src="Images/Bird.jpg" alt="bird" className={className} onClick={selectImage} />
+                    <img src="Images/Cat.jpg" alt="cat" className={className} onClick={selectImage} />
+                    <img src="Images/Dog.jpg" alt="dog" className={className} onClick={selectImage} />
+                    <img src="Images/Girl.jpg" alt="girl" className={className} onClick={selectImage} />
                 </div>
             </div>
         );
